27bdb2670de847168e1dc1a0f754d89bdd877eed
= <iam.ivan.mora@gmail.com>
2018-04-08 21:00:08 -0500

    Created model layer and moved user creation business logic to user model

--- controllers/user.js
+++ controllers/user.js
@@ -4,8 +4,8 @@ const bcrypt = require('bcrypt');
 const uuidv1 = require('uuid/v1');
 const { activeClubUser } = require('./middleware');
 
-const STATUS_ERROR_USER = 422;
-const STATUS_ERROR_SERVER = 500;
+const { user } = require('../models');
+
 const STATUS_SUCCESS = 200;
 
 User.getMembershipsById = function(id) {
@@ -36,104 +36,9 @@ module.exports = function(app) {
 
 		const credentials = req.body;
 	
-		validateRequest(credentials)
-		.then(checkForExisting)
-		.then(generateHash)
-		.then(createUser)
-		.then(sanitizeUser)
+		user.add(credentials)
 		.then(createSuccess)
 		.catch(createFailure);
-
-		function validateRequest(credentials) {
-			return new Promise((resolve, reject) => {
-				const {
-					firstName,
-					lastName,
-					email,
-					password
-				} = credentials;
-
-				if (
-					firstName === undefined ||
-					lastName  === undefined ||
-					email     === undefined ||
-					password  === undefined
-					) {
-					reject({
-						status: STATUS_ERROR_USER,
-						message: 'A first name, last name, email address, and a password must be provided'
-					});
-				} else {
-					resolve(credentials);
-				}
-			});
-		}
-
-		function checkForExisting(credentials) {
-			return new Promise((resolve, reject) => {
-				const {
-					email
-				} = credentials;
-
-				User.findOne({
-					where: { email }
-				})
-				.then(user => {
-					if (user !== null) {
-						reject({
-							status: STATUS_ERROR_USER,
-							message: 'Email address already exists'
-						});
-					} else {
-						resolve(credentials);
-					}
-				})
-			});
-		}
-
-		function generateHash(credentials) {
-			const { password } = credentials;
-			let newCredentials;
-			
-			return new Promise((resolve, reject) => {
-				bcrypt.hash(password, 10, (err, hash) => {
-					if (err) {
-						reject({
-							status: STATUS_ERROR_SERVER,
-							message: err
-						});
-					} else {
-						newCredentials = Object.assign(credentials, { password: hash });
-						resolve(newCredentials);
-					}
-				});
-			});
-		}
-
-		function createUser(credentials) {
-			return User.create(credentials)
-			.then(user => {
-				return user;
-			});
-		}
-
-		function sanitizeUser(user) {
-			const {
-				id,
-				firstName,
-				lastName,
-				email
-			} = user;
-
-			const sanitiezedUser = {
-				id,
-				firstName,
-				lastName,
-				email
-			}
-
-			return sanitiezedUser;
-		}
 
 		function createSuccess(user) {
 			res.status(STATUS_SUCCESS);
@@ -146,8 +51,7 @@ module.exports = function(app) {
 				message
 			} = err;
 
-			console.log(`Status: ${status}`, `Message: ${message}`);
-
+			console.log(`Error adding user: ${status} ${message}`);
 			res.status(status);
 			res.send(message);
 		}
